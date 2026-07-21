bd73f86bd7c201f5037cab0339896084ebfa8c40
PONSHIVAVEL S K <ponshivavelsk@gmail.com>
2026-07-21 19:49:20 +0530

    Add files via upload

---
+++ array/me.java
@@ -0,0 +1,14 @@
+class me{
+    public static void main(String[]args){
+        int n=5;
+        int m=6;
+        int k=2;
+        System.out.println(add(n,m,k));
+    }
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static int add(int a,int b,int c){
+        return a+b+c;
+    }
+}
